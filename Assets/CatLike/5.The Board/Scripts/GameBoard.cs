b63f131c5914acf87d223dc610b6bd5ae645be31
cofdream <cofdream@sina.com>
2021-01-22 18:02:15 +0800

    update

---
+++ Assets/CatLike/5.The Board/Scripts/GameBoard.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoard : MonoBehaviour
+{
+    [SerializeField] Transform ground;
+    [SerializeField] GameTile tilePrefab;
+    Vector2Int size;
+    GameTile[] tileArray;
+    public void Initialize(Vector2Int size)
+    {
+        this.size = size;
+        ground.localScale = new Vector3(size.x, size.y, 1f);
+
+        tileArray = new GameTile[size.x * size.y];
+
+        Vector2 offest = new Vector2((size.x - 1) * 0.5f, (size.y - 1) * 0.5f);
+        for (int y = 0, i = 0; y < size.y; y++)
+        {
+            for (int x = 0; x < size.x; x++, i++)
+            {
+                GameTile tile = tileArray[i] = Instantiate(tilePrefab);
+                tile.transform.SetParent(transform, false);
+                tile.transform.localPosition = new Vector3(x - offest.x, 0, y - offest.y);
+                if (x > 0)
+                {
+                    GameTile.MakeEastWestNeighbors(tile, tileArray[i - 1]);
+                }
+                if (y > 0)
+                {
+                    GameTile.MakeNorthSouthNeighbors(tile, tileArray[i - size.x]);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/CatLike/5.The Board/Scripts/GameTile.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTile : MonoBehaviour
+{
+    [SerializeField] Transform arrow;
+    GameTile north, east, south, west, nextOnPath;
+    int distance;
+    public bool HasPath => distance != int.MaxValue;
+
+
+    public void ClearPath()
+    {
+        distance = int.MaxValue;
+        nextOnPath = null;
+    }
+    public void BecomeDestination()
+    {
+        distance = 0;
+        nextOnPath = null;
+    }
+    private void GrowPathTo(GameTile neighbor)
+    {
+        Debug.Assert(HasPath, "NO path!");
+        if (nextOnPath == null || nextOnPath.HasPath)
+        {
+            return;
+        }
+        neighbor.distance = distance++;
+        neighbor.nextOnPath = this;
+    }
+
+    public static void MakeEastWestNeighbors(GameTile east, GameTile west)
+    {
+        Debug.Assert(west.east == null && east.west == null, "Redefined neighbors!");
+        west.east = east;
+        east.west = west;
+    }
+    public static void MakeNorthSouthNeighbors(GameTile north, GameTile south)
+    {
+        Debug.Assert(south.north == null && north.south == null, "Redefined neighbors!");
+        south.north = north;
+        north.south = south;
+    }
+}
