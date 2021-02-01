4a2ec7bdffeb4c3f00b4e203dfc8d0a596b105f0
cofdream <cofdream@sina.com>
2021-02-01 16:03:31 +0800

    update

---
+++ Assets/CatLike/11.Hex Map/HexCell.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class HexCell : MonoBehaviour
+{
+    public HexCoordinates HexCoordinates;
+    public Color Color;
+}
---
+++ Assets/CatLike/11.Hex Map/HexMapEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMapEditor : MonoBehaviour
+{
+    public Color[] ColorArray = new Color[0];
+    public HexGrid HexGrid;
+
+    private Color activeColor;
+
+    private void Awake()
+    {
+        SelectColor(0);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            HandleInput();
+        }
+    }
+    void HandleInput()
+    {
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(inputRay, out RaycastHit hit))
+        {
+            HexGrid.ColorCell(hit.point, activeColor);
+        }
+    }
+    public void SelectColor(int index)
+    {
+        activeColor = ColorArray[index];
+    }
+}
